e5f2ca4325a0221108c0a314a009b089a01d3b58
zovcharov <egoza9123KGU7v7L6>
2020-03-22 07:34:02 +0300

    Make module reusable for node and browser

--- parser/index.js
+++ parser/index.js
@@ -3,71 +3,85 @@ const utils = require('./utils/utils');
 const baseParserConfig = require('./config/baseParserConfig');
 const logger = require('./logger/logger');
 
-/**
- * Function execute parser and involve callback function
- * @param {string} text text for parsing
- * @param {object} config user configurations
- * @param {function} cb callback function
- */
-function cbParser(text = '', config = {}, cb = () => {}) {
-    try {
-        cb(
-            null, 
-            parserAdapter(text, {
-                logger,
-                ...baseParserConfig,
-                ...utils.prepareConfig(config),
-            })
-        );
-    } catch(err) {
-        cb(err);
-    };
-};
-
-/**
- * Function creates and returns promise for parser
- * @param {string} text text for parsing
- * @param {object} config user configurations
- * @returns {Promise} promise with parser work result
- */
-const promiseParser = (text = '', config = {}) => 
-    new Promise((resolve, reject) => {
+(function() {
+    /**
+     * Function execute parser and involve callback function
+     * @param {string} text text for parsing
+     * @param {object} config user configurations
+     * @param {function} cb callback function
+     */
+    function cbParser(text = '', config = {}, cb = () => {}) {
         try {
-            resolve(parserAdapter(text, {
-                logger,
-                ...baseParserConfig,
-                ...utils.prepareConfig(config),
-            }))
-        } catch (err) {
-            reject(err);
-        }
-    })
+            cb(
+                null, 
+                parserAdapter(text, {
+                    logger,
+                    ...baseParserConfig,
+                    ...utils.prepareConfig(config),
+                })
+            );
+        } catch(err) {
+            cb(err);
+        };
+    };
 
-/**
- * Function check params types and involves nesessary parser type with params
- * @param  {...any} params 
- * @returns {object} parser work result
- */
-const parserFactory = (...params) => {
-    utils.checkParams(...params);
+    /**
+     * Function creates and returns promise for parser
+     * @param {string} text text for parsing
+     * @param {object} config user configurations
+     * @returns {Promise} promise with parser work result
+     */
+    const promiseParser = (text = '', config = {}) => 
+        new Promise((resolve, reject) => {
+            try {
+                resolve(parserAdapter(text, {
+                    logger,
+                    ...baseParserConfig,
+                    ...utils.prepareConfig(config),
+                }))
+            } catch (err) {
+                reject(err);
+            }
+        })
 
-    switch(params.length) {
-        case 1:
-            return promiseParser(params[0]);
-        case 2:
-            if (utils.isObject(params[1])) {
+    /**
+     * Function check params types and involves nesessary parser type with params
+     * @param  {...any} params 
+     * @returns {object} parser work result
+     */
+    const parserFactory = (...params) => {
+        utils.checkParams(...params);
+
+        switch(params.length) {
+            case 1:
+                return promiseParser(params[0]);
+            case 2:
+                if (utils.isObject(params[1])) {
+                    utils.checkConfigObject(params[1]);
+                    return promiseParser(...params);
+                }
+                if (utils.isFunction(params[1])) {
+                    return cbParser(params[0], {}, params[1]);
+                }
+            case 3:
                 utils.checkConfigObject(params[1]);
-                return promiseParser(...params);
-            }
-            if (utils.isFunction(params[1])) {
-                return cbParser(params[0], {}, params[1]);
-            }
-        case 3:
-            utils.checkConfigObject(params[1]);
-            return cbParser(...params)
-        default:
-            return {};
+                return cbParser(...params)
+            default:
+                return {};
+        }
     }
-}
 
-module.exports = parserFactory;
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = parserFactory;
+    }
+    else {
+        if (typeof define === 'function' && define.amd) {
+            define([], function() {
+                return parserFactory;
+            });
+        }
+        else {
+            window.parse = parserFactory;
+        }
+    }
+})();
